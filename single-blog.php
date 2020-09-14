5bdd19b25f9a6aa15bcdc39131fcf0646b9e9442
雨宮祐介 <amamiyayusuke@amamiyayuusukenoMacBook-Pro.local>
2020-09-14 17:43:40 +0900

    renovationSprint09second commit

--- single-blog.php
+++ single-blog.php
@@ -3,7 +3,7 @@ $upload_dir = wp_upload_dir();
 get_header();
 ?>
 
-<div id="content" class="<?php echo $post_type; ?> single">
+<div id="content" class="<?php echo $post_type; ?> single <?php if(is_object_in_term($post->ID, 'blog_category','news'))echo 'news' ;?>">
   <article class="article_main">
     <section id="entry">
       <?php
@@ -19,110 +19,117 @@ get_header();
         }
       } 
       endif;
-      
       ?>
-      <div class="post">
-        <div class="badges">
-          <?php if(is_object_in_term($post->ID, 'blog_category','news')){ ?>
-            <div class="post_badge"><?php echo $term_name; ?></div><div class="date"><?php the_time('Y/n/j'); ?></div>
-          <?php }else{ ?>
-            <div class="post_badge"><?php echo 'Musubiブログ'; ?></div>
-          <?php } ?>
-        </div>
-        <?php
-          while ( have_posts() ) {
-          the_post();
-          $post__not_in = $post->ID;
-        ?>
-        <h1 class="post_tit"><?php the_title(); ?></h1>
-        <div class="post_img">
-          <?php if ( has_post_thumbnail() ) : ?>
-          <img src="<?php the_post_thumbnail_url('full'); ?>" alt="<?php the_title(); ?>">
-          <?php /*else: ?>
-          <img src="/assets/images/common/noimg.jpg" alt="">
-          <?php */endif; ?>
-        </div>
-        
-        <?php if(!is_object_in_term($post->ID, 'blog_category','news')){ ?>
-        <div class="post_summary">
-          <?php if(get_field('cf_summary'))echo '<p class="summary">'.get_field('cf_summary').'</p>'; ?>
-          <div class="post_meta">
-            <?php
-            $terms = get_the_terms( $post->ID, 'blog_category' );
-            if ( $terms && ! is_wp_error( $terms ) ) :
-            ?>
-            <ul class="category">
+      
+      <div class="post_outer <?php if(is_object_in_term($post->ID, 'blog_category','news'))echo 'news' ;?>">
+        <div class="post">
+          <div class="badges">
+            <?php if(is_object_in_term($post->ID, 'blog_category','news')){ ?>
+              <div class="post_badge"><?php echo $term_name; ?></div><div class="date"><?php the_time('Y/n/j'); ?></div>
+            <?php }else{ ?>
+              <div class="post_badge"><?php echo 'Musubiブログ'; ?></div>
+            <?php } ?>
+          </div>
+          <?php
+            while ( have_posts() ) {
+            the_post();
+            $post__not_in = $post->ID;
+          ?>
+          <h1 class="post_tit"><?php the_title(); ?></h1>
+          <div class="post_img">
+            <?php if ( has_post_thumbnail() ) : ?>
+            <img src="<?php the_post_thumbnail_url('full'); ?>" alt="<?php the_title(); ?>">
+            <?php /*else: ?>
+            <img src="/assets/images/common/noimg.jpg" alt="">
+            <?php */endif; ?>
+          </div>
+          
+          <?php if(!is_object_in_term($post->ID, 'blog_category','news')){ ?>
+          <div class="post_summary">
+            <?php if(get_field('cf_summary'))echo '<p class="summary">'.get_field('cf_summary').'</p>'; ?>
+            <div class="post_meta">
               <?php
-                $tags = array();
-                foreach ( $terms as $term ) {
+              $terms = get_the_terms( $post->ID, 'blog_category' );
+              if ( $terms && ! is_wp_error( $terms ) ) :
               ?>
-              <li class="tag"><span class="label"><?php echo $term->name; ?></span></li>
-              <?php } ?>
-            </ul>
-            <?php endif; ?>
-            <div class="date">
-              <?php if ( $terms && ! is_wp_error( $terms ) )echo '| ';
-              the_time('Y/n/j'); ?></div>
-            <?php
+              <ul class="category">
+                <?php
+                  $tags = array();
+                  foreach ( $terms as $term ) {
+                ?>
+                <li class="tag"><span class="label"><?php echo $term->name; ?></span></li>
+                <?php } ?>
+              </ul>
+              <?php endif; ?>
+              <div class="date">
+                <?php if ( $terms && ! is_wp_error( $terms ) )echo '| ';
+                the_time('Y/n/j'); ?></div>
+              <?php
+              $tax = 'blog_tag';
+              $terms = get_the_terms( $post->ID, $tax );
+              if ( $terms && ! is_wp_error( $terms ) ) :
+              ?>
+              <ul class="tags">
+                <?php
+                  foreach ( $terms as $term ) {
+                    echo '<li class="tag"><a href="'.get_term_link($term->slug, $tax).'" class="label">#'.$term->name.'</a></li>';
+                  }
+                ?>
+              </ul>
+              <?php endif; ?>      
+            </div>
+          </div>
+          <?php }else{
             $tax = 'blog_tag';
             $terms = get_the_terms( $post->ID, $tax );
-            if ( $terms && ! is_wp_error( $terms ) ) :
-            ?>
-            <ul class="tags">
-              <?php
-                foreach ( $terms as $term ) {
-                  echo '<li class="tag"><a href="'.get_term_link($term->slug, $tax).'" class="label">#'.$term->name.'</a></li>';
-                }
-              ?>
-            </ul>
-            <?php endif; ?>      
+            if ( $terms && ! is_wp_error( $terms ) || get_field('cf_summary') ) :
+          ?>
+          <div class="post_summary">
+            <?php if(get_field('cf_summary'))echo '<p class="summary">'.get_field('cf_summary').'</p>'; ?>
+            <div class="post_meta">
+              <ul class="tags">
+                <?php
+                  foreach ( $terms as $term ) {
+                    echo '<li class="tag"><a href="'.get_term_link($term->slug, $tax).'" class="label">#'.$term->name.'</a></li>';
+                  }
+                ?>
+              </ul>
+            </div>
           </div>
-        </div>
-        <?php }else{
-          $tax = 'blog_tag';
-          $terms = get_the_terms( $post->ID, $tax );
-          if ( $terms && ! is_wp_error( $terms ) || get_field('cf_summary') ) :
-        ?>
-        <div class="post_summary">
-          <?php if(get_field('cf_summary'))echo '<p class="summary">'.get_field('cf_summary').'</p>'; ?>
-          <div class="post_meta">
-            <ul class="tags">
-              <?php
-                foreach ( $terms as $term ) {
-                  echo '<li class="tag"><a href="'.get_term_link($term->slug, $tax).'" class="label">#'.$term->name.'</a></li>';
-                }
-              ?>
-            </ul>
+          <?php endif;
+            }
+          ?>
+          
+          <div class="post_content">
+            <?php the_content(); ?>
           </div>
+          
+          <?php if(get_field('cf_exlink')){ ?>
+          <div class="post_button"><a href="<?php echo get_field('cf_exlink'); ?>" class="btn cta" target="_blank">掲載ページを見る</a></div>
+          <?php } ?>
+          
+          <?php if(get_field('cf_pdf')){ ?>
+          <div class="post_button"><a href="<?php echo get_field('cf_pdf'); ?>" class="btn cta" target="_blank">PDFダウンロードはこちら</a></div>
+          <?php } ?>
+          
+          <?php } // end while ?>
         </div>
-        <?php endif;
-          }
-        ?>
-        
-        <div class="post_content">
-          <?php the_content(); ?>
-        </div>
-        
-        <?php if(get_field('cf_exlink')){ ?>
-        <div class="post_button"><a href="<?php echo get_field('cf_exlink'); ?>" class="btn cta" target="_blank">掲載ページを見る</a></div>
-        <?php } ?>
-        
-        <?php if(get_field('cf_pdf')){ ?>
-        <div class="post_button"><a href="<?php echo get_field('cf_pdf'); ?>" class="btn cta" target="_blank">PDFダウンロードはこちら</a></div>
-        <?php } ?>
-        
-        <?php } // end while ?>
-      </div>
 
-      <?php
-        else:
-      ?>
-      <div id="nopost">
-        該当する投稿がありません。
-      </div>
-      <?php
-        endif;
-      ?>
+        <?php
+          else:
+        ?>
+        <div id="nopost">
+          該当する投稿がありません。
+        </div>
+        <?php
+          endif;
+        ?>
+        <!-- sidebar（PC） -->
+        <div class="sidebar_outer display-pc">
+        <?php include('sidebar.php'); ?>
+        </div>
+        <!--// sidebar（PC） -->
+      </div><!--// post_outer -->
     </section>
 
     <?php
@@ -186,6 +193,19 @@ get_header();
         ?>
       </ul>
     </section>
+    
+    <!-- sidebar（SP） -->
+    <div class="display-sp">
+      <section id="entry">
+        <div class="post_outer">
+            <div class="sidebar_outer">
+            <?php include('sidebar.php'); ?>
+            </div>
+        </div>
+      </section>
+    </div>
+    <!--// sidebar（SP） -->
+      
     <?php
       endif;
       wp_reset_query();
@@ -197,4 +217,3 @@ get_header();
 <?php get_footer(); ?>
 </body>
 </html>
-
