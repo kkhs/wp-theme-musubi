c5381d116f71c801b235299a94e15b0f7bac7119
雨宮祐介 <amamiyayusuke@amamiyayuusukenoMacBook-Pro.local>
2020-11-09 10:27:17 +0900

    renovationSprint11first_4_pre commit

---
+++ page-support.php
@@ -0,0 +1,68 @@
+<?php get_header(); ?>
+
+<div id="content" class="support">
+  <article class="article_main">
+
+    <section id="top">
+      <div class="title_area">
+        <h1>Musubiのサポート</h1>
+        <p class="sub">なぜシステム選定時にサポート体制が重要視されるのか?</p>
+      </div>
+      <div class="caption_area">
+        <div class="section_inner">
+          <p class="text">
+            システムを変更するには大きな決断が伴います。<br>
+            これまでやってきたことを変えるのは、現場にも負荷がかかります。<br>
+            下記のような様々な不安を抱えているのではないでしょうか?
+          </p>
+        </div>
+      </div>
+      <div class="img_area">
+        <div class="box txt">
+          <p class="check">今使っている電子薬歴からの切り替えが大変そう…</p>
+          <p class="check">今使っている電子薬歴からの切り替えが大変そう…</p>
+          <p class="check">今使っている電子薬歴からの切り替えが大変そう…</p>
+          <p class="check">今使っている電子薬歴からの切り替えが大変そう…</p>
+        </div>
+        <div class="box img">
+          <figure>
+            <img src="" alt="">
+          </figure>
+        </div>
+      </div>
+    </section>  
+
+    <section id="flow">
+      <div class="section_inner">
+        <h2>
+          Musubiは契約後から導入支援の専門スタッフが<br>
+          各フェーズに応じて、不安解消のために寄り添い続けます。
+        </h2>
+        <div class="flow_area">
+          <div class="box">
+            <h3>契約後のヒアリング</h3>
+            <p>今後のスケジュールや、導入に向けた必要事項の再確認で不安な点を解消。</p>
+          </div>
+          <div class="box">
+            <h3>契約後のヒアリング</h3>
+            <p>今後のスケジュールや、導入に向けた必要事項の再確認で不安な点を解消。</p>
+          </div>
+          <div class="box">
+            <h3>契約後のヒアリング</h3>
+            <p>今後のスケジュールや、導入に向けた必要事項の再確認で不安な点を解消。</p>
+          </div>
+          <div class="box">
+            <h3>契約後のヒアリング</h3>
+            <p>今後のスケジュールや、導入に向けた必要事項の再確認で不安な点を解消。</p>
+          </div>
+        </div>
+      </div>
+    </section>  
+
+  </article>
+</div>
+
+<?php get_footer(); ?>
+
+</body>
+</html>              
