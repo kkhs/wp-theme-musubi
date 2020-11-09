0485b9f6ce45d97e0e75d0234f78d1f12540e995
雨宮祐介 <amamiyayusuke@amamiyayuusukenoMacBook-Pro.local>
2020-11-09 17:43:38 +0900

    renovationSprint11first_4_pre_second commit

--- page-support.php
+++ page-support.php
@@ -57,7 +57,467 @@
           </div>
         </div>
       </div>
-    </section>  
+    </section>
+
+    <section id="colum">
+      <div class="section_inner">
+
+        <div class="colum A">
+          <div class="title_area">
+            <div class="icon"><span>1</span></div>
+            <h2>ご契約後のヒアリング</h2>
+          </div>
+          <div class="detail_area">
+            <div class="line">
+              <div class="border"></div>
+            </div>
+            <div class="detail">
+              <div class="focus_area">
+                <div class="title">こんな悩み・不安を解決！</div>
+                <div class="detail">Musubiを使えるようになるまで、どんなスケジュールで進むの?</div>
+              </div>
+              <div class="flex_area">
+                <div class="box">
+                  <h3>適切な導入方法のご提案</h3>
+                  <p class="text">
+                    今後のスケジュールや、導入に際して<br>
+                    必要となる確認事項をヒアリングし、<br>
+                    適切な導入方法をご提案させていただきます。
+                  </p>
+                </div>
+                <div class="box">
+                  <figure>
+                    <img src="" alt="">
+                  </figure>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div><!--// colum A -->
+
+        <div class="colum B">
+          <div class="title_area">
+            <div class="icon"><span>2</span></div>
+            <h2>システム設置時</h2>
+          </div>
+          <div class="detail_area">
+            <div class="line">
+              <div class="border"></div>
+            </div>
+            <div class="detail">
+              <div class="focus_area">
+                <div class="title">こんな悩み・不安を解決！</div>
+                <div class="detail">
+                  今使っているレセコンでも大丈夫？  今使っている電子薬歴からの切り替えが大変そう・・
+                </div>
+              </div>
+              <div class="caption_area">
+                <p>
+                  専門スタッフが店舗へ訪問し、ネットワーク調査からレセコンとの繋ぎこみまで行います。<br>
+                  半日程度で導入設置は完了します。（通常は90分程度）
+                </p>
+              </div>
+              <div class="flex_area colum_wh">
+                <div class="box">
+                  <h3>お使いのレセコンと連携が可能</h3>
+                  <p class="caption">
+                    Musubiは日本薬剤師会が推奨する、薬局向けコンピュータ間の
+                    連携システムであるNSIPS®に準拠しております。
+                    NSIPS®に対応しているレセコンとの連携が可能です。
+                    多数のレセコンメーカーとの連携実績がありますのでご安心ください。
+                  </p>
+                  <p class="sub">
+                    ＊NSIPSRとは日本薬剤師会が提案する、薬局向けの各種調剤システムを連動させるための共有仕様です。
+                    ＊NSIPSRは公益社団法人日本薬剤師会の登録商標です。（商標登録 第5214610号）
+                  </p>
+                </div>
+                <div class="box img">
+                  <figure>
+                    <img src="" alt="">
+                  </figure>
+                </div>
+              </div>
+              <div class="flex_area colum_wh">
+                <div class="box">
+                  <h3>
+                    過去データの移行や、新規患者の<br>
+                    データ入力の手間を軽減
+                  </h3>
+                  <p class="focus">「過去データ移行サービス」</p>
+                  <p class="caption">
+                    exPDで仕様が指定されているデータについて、Musubiへのインポートが可能です。データ移行が出来ないメーカーもございますので、詳細はお問い合わせください。
+                  </p>
+                  <p class="focus">「問診票OCRサービス」</p>
+                  <p class="caption">
+                    システム切り替え時に新規取得する問診票や新規患者さんの問診票を
+                    スキャナで自動読み込み。データを手打ち入力する手間を軽減します。
+                  </p>
+                </div>
+                <div class="box img flex_end">
+                  <figure class="mb">
+                    <img src="" alt="">
+                  </figure>
+                  <figure>
+                    <img src="" alt="">
+                  </figure>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div><!--// colum B -->
+
+        <div class="colum C">
+          <div class="title_area">
+            <div class="icon"><span>3</span></div>
+            <h2>導入前後</h2>
+          </div>
+          <div class="detail_area">
+
+            <div class="line">
+              <div class="border"></div>
+            </div>
+            <div class="detail">
+              <div class="focus_area">
+                <div class="title">こんな悩み・不安を解決！</div>
+                <div class="detail">スタッフ全員が使いこなせる？ パソコン操作に自信がないけれど大丈夫…？</div>
+              </div>
+              <div class="flex_area">
+                <div class="box">
+                  <h3>Musubi定着プログラム（Manabi）</h3>
+                  <p class="text">
+                    売って終わりではなく、薬局の先生方がMusubiを使いこなし、<br>
+                    地域の患者さんへ価値をお届けするまでをサポートします。
+                  </p>
+                </div>
+                <div class="box">
+                  <figure>
+                    <img src="" alt="">
+                  </figure>
+                </div>
+              </div>
+              
+              <div class="flow_area">
+                <p class="title">具体的な習得へのステップ</p>
+                <div class="flow">
+                  <div class="box txt">
+                    <h4>キックオフ</h4>
+                    <div class="caption_area">
+                      <div class="arrow_area">
+                        <span class="arrow"></span>
+                      </div>
+                      <div class="colum_wh">
+                        <h5>ヒアリング・導入意義浸透・目標設定</h5>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="box img">
+                    <figure></figure>
+                  </div>
+                </div><!--// flow_1 -->
+                <div class="flow">
+                  <div class="box txt">
+                    <h4>Musubiご利用開始前</h4>
+                    <div class="caption_area">
+                      <div class="arrow_area">
+                        <span class="arrow"></span>
+                      </div>
+                      <div class="colum_wh">
+                        <h5>基本操作をマスター</h5>
+                        <p>
+                          一回きりの全体研修ではなく、マンツーマンサポートで<br>
+                          繰り返しレクチャーを行いながら定着状況を確認
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="box img">
+                    <figure></figure>
+                  </div>
+                </div><!--// flow_2 -->
+                <div class="flow">
+                  <div class="box txt">
+                    <h4>Musubiご利用開始後</h4>
+                    <div class="caption_area">
+                      <div class="arrow_area">
+                        <span class="arrow"></span>
+                      </div>
+                      <div class="colum_wh">
+                        <h5>疑問点の解消と応用術</h5>
+                        <p>
+                          業務開始後に生じた疑問点も解消でき、<br>
+                          さらなるMusubiの活用をサポート
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="box img">
+                    <figure></figure>
+                  </div>
+                </div><!--// flow_3 -->
+                <div class="flow">
+                  <div class="box txt">
+                    <h4>ご卒業</h4>
+                    <div class="caption_area">
+                      <div class="colum_wh last">
+                        <p>
+                          ご卒業後も、患者さんへのさらなる価値提供と<br>
+                          業務効率化の実現に向けて、継続的なサポートを行います。
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="box img">
+                    <figure></figure>
+                  </div>
+                </div><!--// flow_4 -->
+              </div><!--// flow_area -->
+
+              <div class="voice_area">
+                <h3>お客様の声</h3>
+                <div class="voice_base">
+                  <div class="icon">
+                    <figure></figure>
+                  </div>
+                  <div class="voice">
+                    <p>毎回、私のレベルにあった説明や時間配分をしていただいて助かりました</p>
+                  </div>
+                </div>
+                <div class="voice_base">
+                  <div class="icon">
+                    <figure></figure>
+                  </div>
+                  <div class="voice">
+                    <p>毎回、私のレベルにあった説明や時間配分をしていただいて助かりました</p>
+                  </div>
+                </div>
+                <div class="voice_base">
+                  <div class="icon">
+                    <figure></figure>
+                  </div>
+                  <div class="voice">
+                    <p>毎回、私のレベルにあった説明や時間配分をしていただいて助かりました</p>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div><!--// colum C -->
+
+        <div class="colum D">
+          <div class="title_area">
+            <div class="icon"><span>4</span></div>
+            <h2>導入後</h2>
+          </div>
+          <div class="detail_area">
+            <div class="line">
+              <div class="border"></div>
+            </div>
+            <div class="detail">
+              <div class="focus_area">
+                <div class="title">こんな悩み・不安を解決！</div>
+                <div class="detail">
+                  万一トラブルが起きたら？  気づいた時に即疑問を解消したい
+                </div>
+              </div>
+              <div class="caption_area">
+                <p>
+                導入完了後も、サポートデスク・サポートサイトで常時対応。お困りごとや疑問を、その場ですぐに解決できます。
+                </p>
+              </div>
+
+              <div class="flex_area colum_wh support">
+                <div class="box large">
+                  <h3>万が一のトラブルでも安心！</h3>
+                  <ul class="list">
+                    <li>不具合等のご相談</li>
+                    <li>操作方法のご質問</li>
+                    <li>システムに対するご要望</li>
+                  </ul>
+                  <p class="caption">
+                    応答率 97.0%、一次解決率 84.0%  という高い水準で、疑問をスムーズに解決！<br>
+                    Musubiに関するお困りごとは等は、サポートデスクへお電話ください！
+                  </p>
+                  <p class="sub">
+                    ※1 サポートデスク対応後アンケート調査結果より　　※2 弊社サポートデスク 集計データ(2020年9月末実績) 　
+                  </p>
+                </div>
+                <div class="box small">
+                  <figure>
+                    <img src="" alt="">
+                  </figure>
+                </div>
+              </div>
+
+              <div class="flex_area colum_wh support">
+                <div class="box large">
+                  <h3>気づいた時に、すぐに疑問を解消できる！</h3>
+                  <ul class="list">
+                    <li>よくある質問</li>
+                    <li>Musubi操作マニュアル</li>
+                    <li>リリース情報·メンテナンス情報</li>
+                    <li>ユーザー限定のイベント情報や過去セミナー動画</li>
+                  </ul>
+                  <p class="caption">
+                    充実したサポートサイトで、いつでも使い方の確認が出来ます！
+                  </p>
+                </div>
+                <div class="box small">
+                  <figure>
+                    <img src="" alt="">
+                  </figure>
+                </div>
+              </div>
+
+              <div class="voice_area">
+                <h3>お客様の声</h3>
+                <div class="voice_base">
+                  <div class="icon">
+                    <figure></figure>
+                  </div>
+                  <div class="voice">
+                    <p>毎回、私のレベルにあった説明や時間配分をしていただいて助かりました</p>
+                  </div>
+                </div>
+                <div class="voice_base">
+                  <div class="icon">
+                    <figure></figure>
+                  </div>
+                  <div class="voice">
+                    <p>毎回、私のレベルにあった説明や時間配分をしていただいて助かりました</p>
+                  </div>
+                </div>
+                <div class="voice_base">
+                  <div class="icon">
+                    <figure></figure>
+                  </div>
+                  <div class="voice">
+                    <p>毎回、私のレベルにあった説明や時間配分をしていただいて助かりました</p>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div><!--// colum D -->
+
+        <div class="colum plus">
+          <div class="title_area">
+            <div class="icon"><span>α</span></div>
+            <h2>ユーザーセミナー・イベント・コミュニティ</h2>
+          </div>
+          <div class="detail_area">
+            <div class="line">
+              <div class="border"></div>
+            </div>
+
+            <div class="detail">
+              <div class="focus_area">
+                <div class="title">こんな悩み・不安を解決！</div>
+                <div class="detail">スキルアップもしたい...　薬剤師同士で語りたい…</div>
+              </div>
+              <div class="flex_area">
+                <div class="box small">
+                  <p class="text">
+                    ユーザ限定の講座・交流会を定期的に開催。更なるスキルアップや薬剤師同士の意見交換など、製品のサポートだけではないMusubiだから出来る取り組みです。
+                  </p>
+                  <div class="colum_wh">
+                    <h3>スキルアップを目指せる！</h3>
+                    <p class="text">
+                      漢方実践講座、服用期間中フォローの実践講座、服薬情報提供書講座など···<br>
+                      各分野専門の講師をお招きして定期的にセミナーを開催しております。もちろん参加費は無料です。
+                    </p>
+                    <h3>薬剤師同士の交流ができる！</h3>
+                    <p class="text last">
+                      ユーザーイベントは全国からMusubiを使っているユーザー様があつまって、自局の課題や解決策を共有したり、意見交換をして頂いて大変好評いただいております。
+                    </p>
+                  </div>
+                </div>
+                <div class="box large">
+                  <figure>
+                    <img src="" alt="">
+                  </figure>
+                </div>
+              </div>
+
+              <div class="voice_area">
+                <h3>参加者の声</h3>
+                <div class="voice_base">
+                  <div class="icon">
+                    <figure></figure>
+                  </div>
+                  <div class="voice">
+                    <p>毎回、私のレベルにあった説明や時間配分をしていただいて助かりました</p>
+                  </div>
+                </div>
+                <div class="voice_base">
+                  <div class="icon">
+                    <figure></figure>
+                  </div>
+                  <div class="voice">
+                    <p>毎回、私のレベルにあった説明や時間配分をしていただいて助かりました</p>
+                  </div>
+                </div>
+                <div class="voice_base">
+                  <div class="icon">
+                    <figure></figure>
+                  </div>
+                  <div class="voice">
+                    <p>毎回、私のレベルにあった説明や時間配分をしていただいて助かりました</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="link_area">
+                <ul>
+                  <li><a href="#">Musubiユーザー50人が一堂に集結！「Musubi Meetup #01」を開催しました</a></li>
+                  <li><a href="#">ここが変わった！Musubi導入前/導入後～Musubiを薬局内に浸透させるために取り組んだこと～[Musubi Meetup #01]</a></li>
+                  <li><a href="#">薬局の「外」にもっと価値を届けたい～創業50年の老舗薬局で、Musubi導入後に見えた変化とは～[Musubi Meetup #01]</a></li>
+                </ul>
+              </div>
+
+            </div>
+
+          </div>
+        </div><!--// colum plus -->
+
+      </div>
+    </section>
+
+    <section id="cta">
+      <div class="section_inner">
+        <div class="cta_area setElm">
+          <h2>さあ、選ばれる薬局へ</h2>
+          <p class="point">Musubiが選ばれる理由</p>
+          <div class="cta_box">
+            <div class="box first">
+              <figure>
+              <img src="<?php echo_assets_root_url(); ?>assets/images/product/custom/img_cta_1_l.png" alt="質を伴った業務効率化">
+              </figure>
+              <h3>質を伴った<br class="display-pc">業務効率化</h3>
+              <p>服薬指導の内容が、自動で薬歴へ。外来でも在宅でも、より深く患者さんに向き合うことができます。</p>
+            </div>
+            <div class="box sec">
+              <figure>
+              <img src="<?php echo_assets_root_url(); ?>assets/images/product/custom/img_cta_2_l.png" alt="シンプルな画面操作方法">
+              </figure>
+              <h3>シンプルな画面<br class="display-pc">操作方法</h3>
+              <p>従来のレセコンや電子薬歴とは一線を画すシンプルな画面に、画面タッチ中心の操作。使う人を選びません。</p>
+            </div>
+            <div class="box thi">
+              <figure>
+              <img src="<?php echo_assets_root_url(); ?>assets/images/product/custom/img_cta_3_l.png" alt="マンツーマンサポート">
+              </figure>
+              <h3>マンツーマン<br class="display-pc">サポート</h3>
+              <p>満足度90％以上。継続的なMusubiの活用サポートで、PC操作やITシステムが苦手な薬剤師さんも安心です。</p>
+            </div>
+          </div>
+        </div>
+        <div class="btn_area">
+          <a href="/contact" class="btn cta">お気軽にお問い合わせください</a>
+        </div>
+      </div>
+    </section>
 
   </article>
 </div>
