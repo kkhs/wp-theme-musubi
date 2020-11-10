1216dce124cf3bb828dd27457744754f70d553e5
雨宮祐介 <amamiyayusuke@amamiyayuusukenoMacBook-Pro.local>
2020-11-10 10:35:39 +0900

    renovationSprint11first_3_forth commit

--- page-contact.php
+++ page-contact.php
@@ -20,7 +20,7 @@ get_header();
                   <span class="num">1</span>
                   <h3>お問い合わせ</h3>
                   <figure>
-                    <img src="<?php echo_assets_root_url(); ?>assets/images/contact/img_contact_flow_1@2x.png" alt="">
+                    <img src="<?php echo_assets_root_url(); ?>assets/images/contact/img_contact_flow_1@2x.png" alt="流れ１">
                   </figure>
                   <p>まずは下記のフォームより、お気軽にご連絡ください。</p>
                 </div>
@@ -28,7 +28,7 @@ get_header();
                   <span class="num">2</span>
                   <h3>ご連絡・お打ち合わせ</h3>
                   <figure>
-                    <img src="<?php echo_assets_root_url(); ?>assets/images/contact/img_contact_flow_2@2x.png" alt="">
+                    <img src="<?php echo_assets_root_url(); ?>assets/images/contact/img_contact_flow_2@2x.png" alt="流れ２">
                   </figure>
                   <p>適宜、お打ち合わせをご提案し、ヒアリングいたします。</p>
                 </div>
@@ -36,7 +36,7 @@ get_header();
                 <span class="num">3</span>
                   <h3>デモ実演・商談</h3>
                   <figure>
-                    <img src="<?php echo_assets_root_url(); ?>assets/images/contact/img_contact_flow_3@2x.png" alt="">
+                    <img src="<?php echo_assets_root_url(); ?>assets/images/contact/img_contact_flow_3@2x.png" alt="流れ３">
                   </figure>
                   <p>導入判断に必要な具体的な情報をご提供いたします。</p>
                 </div>
@@ -44,7 +44,7 @@ get_header();
                 <span class="num">4</span>
                   <h3>ご契約</h3><span></span>
                   <figure>
-                    <img src="<?php echo_assets_root_url(); ?>assets/images/contact/img_contact_flow_4@2x.png" alt="">
+                    <img src="<?php echo_assets_root_url(); ?>assets/images/contact/img_contact_flow_4@2x.png" alt="流れ４">
                   </figure>
                   <p>商談後、双方が合意の上でご契約いたします。</p>
                 </div>
